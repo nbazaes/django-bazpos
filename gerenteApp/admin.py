9c0ff2cb22722494ad35562024d1ab03624552de
Nicolás Bazaes <contacto@bazaes.cl>
2026-06-02 16:37:27 -0400

    feat: activate Django admin as superuser-only panel with dark theme

--- gerenteApp/admin.py
+++ gerenteApp/admin.py
@@ -1,6 +1,60 @@
 from django.contrib import admin
-from gerenteApp.models import Tax
+from gerenteApp.models import Proveedor, Factura, DetalleFactura, PrecioHistorico, Tax
+
+
+class SuperuserOnlyAdmin(admin.ModelAdmin):
+    def has_module_permission(self, request):
+        return request.user.is_active and request.user.is_superuser
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_active and request.user.is_superuser
+
+    def has_add_permission(self, request):
+        return request.user.is_active and request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_active and request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_active and request.user.is_superuser
+
+
+class DetalleFacturaInline(admin.TabularInline):
+    model = DetalleFactura
+    extra = 0
+    fields = ('producto', 'cantidad', 'costo_compra')
+
+    def has_add_permission(self, request, obj=None):
+        return request.user.is_active and request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_active and request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_active and request.user.is_superuser
+
+
+@admin.register(Proveedor)
+class ProveedorAdmin(SuperuserOnlyAdmin):
+    list_display = ('proveedor_id', 'rut', 'nombre', 'persona_contacto', 'telefono', 'correo', 'fecha_creacion')
+    search_fields = ('rut', 'nombre')
+
+
+@admin.register(Factura)
+class FacturaAdmin(SuperuserOnlyAdmin):
+    list_display = ('numero_factura', 'proveedor', 'fecha', 'monto_total')
+    list_filter = ('fecha', 'proveedor')
+    search_fields = ('numero_factura',)
+    inlines = [DetalleFacturaInline]
+
+
+@admin.register(PrecioHistorico)
+class PrecioHistoricoAdmin(SuperuserOnlyAdmin):
+    list_display = ('producto', 'fecha', 'precio_costo_anterior', 'precio_costo_nuevo', 'precio_venta_anterior', 'precio_venta_nuevo')
+    list_filter = ('fecha',)
+    readonly_fields = ('producto', 'precio_costo_anterior', 'precio_costo_nuevo', 'precio_venta_anterior', 'precio_venta_nuevo', 'fecha', 'factura')
+
 
 @admin.register(Tax)
-class TaxAdmin(admin.ModelAdmin):
+class TaxAdmin(SuperuserOnlyAdmin):
     list_display = ("id", "tax_percent")
--- vendedorApp/admin.py
+++ vendedorApp/admin.py
@@ -1,3 +1,70 @@
 from django.contrib import admin
+from vendedorApp.models import Ubicacion, Producto, StockProductoUbicacion, Venta, DetalleVenta
 
-# Register your models here.
+
+class SuperuserOnlyAdmin(admin.ModelAdmin):
+    def has_module_permission(self, request):
+        return request.user.is_active and request.user.is_superuser
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_active and request.user.is_superuser
+
+    def has_add_permission(self, request):
+        return request.user.is_active and request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_active and request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_active and request.user.is_superuser
+
+
+class StockProductoUbicacionInline(admin.TabularInline):
+    model = StockProductoUbicacion
+    extra = 0
+    fields = ('ubicacion', 'cantidad')
+
+    def has_add_permission(self, request, obj=None):
+        return request.user.is_active and request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_active and request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_active and request.user.is_superuser
+
+
+class DetalleVentaInline(admin.TabularInline):
+    model = DetalleVenta
+    extra = 0
+    fields = ('producto', 'cantidad', 'precio_unitario', 'subtotal')
+
+    def has_add_permission(self, request, obj=None):
+        return request.user.is_active and request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_active and request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_active and request.user.is_superuser
+
+
+@admin.register(Ubicacion)
+class UbicacionAdmin(SuperuserOnlyAdmin):
+    list_display = ('id', 'nombre', 'marca')
+    search_fields = ('nombre',)
+
+
+@admin.register(Producto)
+class ProductoAdmin(SuperuserOnlyAdmin):
+    list_display = ('producto_id', 'nombre', 'codigo_producto', 'marca', 'precio_costo', 'precio', 'stock_minimo', 'stock_maximo', 'proveedor')
+    search_fields = ('nombre', 'codigo_producto')
+    list_filter = ('proveedor', 'marca')
+    inlines = [StockProductoUbicacionInline]
+
+
+@admin.register(Venta)
+class VentaAdmin(SuperuserOnlyAdmin):
+    list_display = ('id', 'usuario', 'fecha_venta', 'monto_total', 'estado', 'tipo_documento')
+    list_filter = ('estado', 'tipo_documento', 'fecha_venta')
+    inlines = [DetalleVentaInline]
